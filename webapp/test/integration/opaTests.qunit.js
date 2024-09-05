d8a1a1fa039ea27022f3f40a9b3bb71cb93c0c8f
George280400 <jch127518@gmail.com>
2024-09-05 22:01:05 +0000

    First Commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["productsjson/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"productsjson/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
